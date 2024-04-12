3d34ecb7839b4af2ebee500e88d433284ab0e4c7
Matthew Kwon <matthew.o.kwon@gmail.com>
2024-04-11 17:54:31 -0700

    Designed schema for Car object

--- src/main/java/com/revature/entity/Car.java
+++ src/main/java/com/revature/entity/Car.java
@@ -1,2 +1,30 @@
-package.com.revature.models;
+package com.example.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @Column(name = "carId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String make;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(nullable = false)
+    private int year;
+
+    @Column(nullable = false)
+    private String color;
+
+    @Column(nullable = true)
+    private String specificDetails;
+
+
+}
