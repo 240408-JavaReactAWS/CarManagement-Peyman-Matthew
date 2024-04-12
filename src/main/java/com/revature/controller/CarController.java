c7c6f00cc8944a8be7f00e546c9440d67b86dd54
Matthew Kwon <matthew.o.kwon@gmail.com>
2024-04-12 11:01:00 -0700

    Implemented the Controller layer for Car and User

---
+++ src/main/java/com/revature/controller/CarController.java
@@ -0,0 +1,68 @@
+package com.revature.controller;
+
+import com.revature.entity.Car;
+import com.revature.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+
+    private CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    // Create car
+    @PostMapping
+    public ResponseEntity<Car> createCar(@RequestBody Car car) {
+        Car newCar = carService.createCar(car);
+        return ResponseEntity.ok(newCar);
+    }
+
+    // View all cars
+    @GetMapping
+    public ResponseEntity<List<Car>> getAllCars() {
+        List<Car> cars = (List<Car>) carService.getAllCars();
+        return ResponseEntity.ok(cars);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Car> getCarById(@PathVariable Integer id) {
+        Optional<Car> carOptional = carService.findCarById(id);
+        if (carOptional.isPresent()) {
+            return ResponseEntity.ok(carOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Update car by ID
+    @PutMapping("/{id}")
+    public ResponseEntity<Car> updateCar(@PathVariable Integer id, @RequestBody Car car) {
+        try {
+            Car updatedCar = carService.updateCar(id, car);
+            return ResponseEntity.ok(updatedCar);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Delete car by ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCar(@PathVariable Integer id) {
+        try {
+            carService.deleteCar(id);
+            return ResponseEntity.ok().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/com/revature/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.revature.controller;
+
+import com.revature.entity.User;
+import com.revature.entity.Car;
+import com.revature.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Create/login user
+    @PostMapping("/register")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User newUser = userService.createUser(user);
+        return ResponseEntity.ok(newUser);
+    }
+
+    // Login
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestParam String username, @RequestParam String password) {
+        return userService.login(username, password)
+                .map(user -> ResponseEntity.ok().build()) // Ideally return a token or user details
+                .orElseGet(() -> ResponseEntity.badRequest().body("Invalid username or password"));
+    }
+
+    // Get favorite cars
+    @GetMapping("/{userId}/favorite-cars")
+    public ResponseEntity<Set<Car>> getUserFavoriteCars(@PathVariable Integer userId) {
+        try {
+            Set<Car> favoriteCars = userService.getUserFavoriteCars(userId);
+            return ResponseEntity.ok(favoriteCars);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Get ALL favorite cars (if Admin)
+    @GetMapping("/favorite-cars/all")
+    public ResponseEntity<Set<Car>> getAllUsersFavoriteCars(@RequestHeader("X-User-Admin") boolean isAdmin) {
+        if (!isAdmin) {
+            return ResponseEntity.status(403).body(null); // Forbidden access
+        }
+        Set<Car> allFavoriteCars = userService.getAllUsersFavoriteCars();
+        return ResponseEntity.ok(allFavoriteCars);
+    }
+}
