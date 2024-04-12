063fcf6d220445226e0b05b524d9e869ed58ac03
Matthew Kwon <matthew.o.kwon@gmail.com>
2024-04-11 19:05:35 -0700

    Implemented service layer for Cars

---
+++ src/main/java/com/revature/service/CarService.java
@@ -0,0 +1,51 @@
+package com.revature.service;
+
+import com.revature.exception.CarNotFoundException;
+import com.revature.entity.Car;
+import com.revature.repository.CarDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public class CarService {
+
+    private CarDAO carDAO;
+
+    @Autowired
+    public CarService(CarDAO carDAO) {
+        this.carDAO = carDAO;
+    }
+
+    // Create car
+    public Car createCar(Car car) {
+        return carDAO.save(car);
+    }
+    // View all cars
+    public Car getAllCars() {
+        return carDAO.findAll();
+    }
+
+    // View car by ID
+    public Optional<Car> findCarById(Integer id) {
+        return carRepository.findById(id);
+    }
+
+    // Update car by Id
+    public Car updateCar(Integer id, Car updatedCar) {
+        return carDAO.findById(id)
+                .map(car -> {
+                    car.setMake(updatedCar.getMake());
+                    car.setModel(updatedCar.getModel());
+                    car.setYear(updatedCar.getYear());
+                    car.setColor(updatedCar.getColor());
+                    car.setSpecificDetails(updatedCar.getSpecificDetails());
+                    return carDAO.save(car);
+                }).orElseThrow(() -> new RuntimeException("Car not found"));
+    }
+
+    // Delete car by Id
+    public void deleteCar(Integer id) {
+        carDAO.deleteById(id);
+    }
+}
